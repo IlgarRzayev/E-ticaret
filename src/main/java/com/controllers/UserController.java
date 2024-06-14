cfaaad6d5d4ae1c33bc0ee36c1d9cc888a4d420b
IlgarRzayev <92079654+IlgarRzayev@users.noreply.github.com>
2024-06-14 03:13:28 +0300

    Update UserController.java

--- src/main/java/com/controllers/UserController.java
+++ src/main/java/com/controllers/UserController.java
@@ -11,10 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.beans.User;
 import com.dao.UserDao;
 
+
 @Controller
 public class UserController {
 	@Autowired
-	UserDao dao;// will inject dao from XML file
+	UserDao udao;// will inject dao from XML file
 
 	@RequestMapping("/")
 	public String index(Model m) {
@@ -31,6 +32,7 @@ public class UserController {
 		m.addAttribute("command", new User());
 		return "register";
 	}
+	
 
 	/*
 	 * It saves object into database. The @ModelAttribute puts request data into
@@ -39,8 +41,35 @@ public class UserController {
 	 */
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(@ModelAttribute("user") User user) {
-		dao.save(user);
-		return "redirect:/homepage";// will redirect to viewemp request mapping
+		udao.save(user);
+		return "homepage_foruser";// will redirect to viewemp request mapping
 	}
+	/* It provides list of employees in model object */    
+    @RequestMapping("/viewemp")    
+    public String viewemp(Model m){    
+        List<User> list=udao.getUsers();    
+        m.addAttribute("list",list);  
+        return "viewemp";   
+    }    
+    /* It displays object data into form for the given id.   
+     * The @PathVariable puts URL data into variable.*/    
+    @RequestMapping(value="/editemp/{id}")    
+    public String edit(@PathVariable int id, Model m){    
+        User user=udao.getUserById(id);    
+        m.addAttribute("command",user);  
+        return "empeditform";    
+    }    
+    /* It updates model object. */    
+    @RequestMapping(value="/editsave",method = RequestMethod.POST)    
+    public String editsave(@ModelAttribute("user") User user){    
+        udao.update(user);    
+        return "redirect:/viewemp";    
+    }    
+    /* It deletes record for the given id in URL and redirects to /viewemp */    
+    @RequestMapping(value="/deleteuser/{id}",method = RequestMethod.GET)    
+    public String delete(@PathVariable int id){    
+        udao.delete(id);    
+        return "redirect:/viewemp";    
+    }
 	
 }
