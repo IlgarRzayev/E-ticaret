bebc2eb2cb1a09984c9eee7abaa5d6bc55a5de0e
IlgarRzayev <92079654+IlgarRzayev@users.noreply.github.com>
2024-06-14 03:14:17 +0300

    Update ProductController.java

--- src/main/java/com/controllers/ProductController.java
+++ src/main/java/com/controllers/ProductController.java
@@ -6,20 +6,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
 import com.beans.Product;
 import com.dao.ProductDao;
 import com.dao.UserDao;
 
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.List;
 
 @Controller
 public class ProductController {
 	@Autowired
-	ProductDao dao;
+	ProductDao pdao;
 	
     @GetMapping("/add-product")
     public String showAddProductForm(Model model) {
@@ -48,9 +52,22 @@ public class ProductController {
                 e.printStackTrace();
             }
         }
-        dao.save(product);
+        pdao.save(product);
         return "redirect:/profile";
     }
+    @RequestMapping("/profile")    
+    public String viewproduct(Model m){    
+        List<Product> list=pdao.getProducts();    
+        m.addAttribute("list",list);  
+        return "profile";   
+    }
+    @RequestMapping("/shareproduct/{productId}")    
+    public String edit(@PathVariable int productId, Model m){    
+        Product product =pdao.getProductById(productId);    
+        m.addAttribute("command",product);  
+        return "homepage_foruser";    
+    }
+    
 
     
 }
