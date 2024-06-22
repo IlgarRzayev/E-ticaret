5d2e9b5f8be20777d364410ad9f5db362ec184a0
IlgarRzayev <92079654+IlgarRzayev@users.noreply.github.com>
2024-06-22 07:35:53 +0300

    Update OrderDao.java

--- src/main/java/com/dao/OrderDao.java
+++ src/main/java/com/dao/OrderDao.java
@@ -1,9 +1,11 @@
 package com.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.beans.Order;
+import com.beans.User;
 
 public class OrderDao {	
 	@Autowired
@@ -13,8 +15,17 @@ public class OrderDao {
 	    this.template = template;    
 	}
 	
-	public int save(Order order) {
+	public int addOrder(Order order) {
 		String sql = "insert into orders(productId, userId, count, paymentMethod) values('" + order.getProductId() + "','" + order.getUserId() + "','" + order.getCount() + "','" + order.getPaymentMethod() + "')";    
 	    return template.update(sql);
 	}
+	public Order getOrdersByUserId(int userId) {
+	    String sql = "select * from orders where userId=?";
+	    try {
+	        return template.queryForObject(sql, new Object[]{userId}, new BeanPropertyRowMapper<>(Order.class));
+	    } catch (Exception e) {
+	        return null;
+	    }
+	}
 }	
+	
