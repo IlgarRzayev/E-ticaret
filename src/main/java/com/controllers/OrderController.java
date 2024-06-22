e84631b4e2acdcd6cf849cb0a6c1a60f3c4b7e9f
IlgarRzayev <92079654+IlgarRzayev@users.noreply.github.com>
2024-06-22 07:34:55 +0300

    Update OrderController.java

--- src/main/java/com/controllers/OrderController.java
+++ src/main/java/com/controllers/OrderController.java
@@ -10,20 +10,61 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.*;
+
+import com.beans.Cart;
 import com.beans.Order;
 import com.beans.Product;
+import com.beans.User;
+import com.dao.OrderDao;
 import com.dao.ProductDao;
 
 @Controller
 public class OrderController {
     @Autowired
+    OrderDao orderdao;
+    @Autowired
     ProductDao pdao;
 
-    @RequestMapping("/order")
-    public String showform(Model m) {
-        m.addAttribute("command", new Order());
-        return "orderpage";
+    @RequestMapping(value = "/pay/{productId}", method = RequestMethod.GET)
+    public String addToCart(@PathVariable int productId, HttpSession session, Model m) {
+        User user = (User) session.getAttribute("loggedInUser");
+        
+        
+        Order order = new Order();
+        order.setUserId(user.getId());
+        order.setProductId(productId);
+        orderdao.addOrder(order);
+
+        return "redirect:/cart";
     }
+    
+    
+    
+    @RequestMapping("/order")
+        public String viewOrder(HttpSession session, Model m) {
+    	User user = (User) session.getAttribute("loggedInUser");
+
+        if (user == null) {
+            return "redirect:/register";
+        }
+
+        List<Order> orders = (List<Order>) orderdao.getOrdersByUserId(user.getId());
+        List<Product> products = new ArrayList<>();
+
+        for (Order order : orders) {
+            Product product = pdao.getProductById(order.getProductId());
+            products.add(product);
+        }
+
+        m.addAttribute("orders", orders);
+        m.addAttribute("products", products);
+
+        return "cart";
+        
+    }
+    
+    
 
     
 }
+
