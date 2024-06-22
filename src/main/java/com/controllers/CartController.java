3b9dc679c40b8d57ec374e8b95a0532225a66d60
IlgarRzayev <92079654+IlgarRzayev@users.noreply.github.com>
2024-06-22 07:21:33 +0300

    Update CartController.java

--- src/main/java/com/controllers/CartController.java
+++ src/main/java/com/controllers/CartController.java
@@ -1,14 +1,17 @@
 package com.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.beans.Cart;
+import com.beans.Product;
 import com.beans.User;
 import com.dao.CartDao;
 import com.dao.ProductDao;
@@ -25,39 +28,48 @@ public class CartController {
 	    public String addToCart(@PathVariable int productId, HttpSession session, Model m) {
 	        User user = (User) session.getAttribute("loggedInUser");
 	        if (user == null) {
-	            return "redirect:/login";
+	            return "redirect:/register";
 	        }
 
 	        Cart cartItem = new Cart();
 	        cartItem.setUserId(user.getId());
 	        cartItem.setProductId(productId);
 	        cartItem.setQuantity(1); // Default quantity
+	        
 	        cartdao.addCartItem(cartItem);
-
+	        
+	        
 	        return "redirect:/cart";
 	    }
 
 	    @RequestMapping("/cart")
 	    public String viewCart(HttpSession session, Model m) {
 	        User user = (User) session.getAttribute("loggedInUser");
+
 	        if (user == null) {
 	            return "redirect:/register";
 	        }
 
 	        List<Cart> cartItems = cartdao.getCartItemsByUserId(user.getId());
+	        List<Product> products = new ArrayList<>();
+
+	        for (Cart item : cartItems) {
+	            Product product = pdao.getProductById(item.getProductId());
+	            products.add(product);
+	        }
+
 	        m.addAttribute("cartItems", cartItems);
+	        m.addAttribute("products", products);
+
 	        return "cart";
 	    }
-
-	    @RequestMapping("/login")
-	    public String login(HttpSession session, Model m) {
-	        // Login işlemleri burada yapılır.
-	        // Giriş başarılı olduğunda:
-	        User user = (User) session.getAttribute("loggedInUser");
-	        if (user != null) {
-	            List<Cart> cartItems = cartdao.getCartItemsByUserId(user.getId());
-	            session.setAttribute("cartItems", cartItems);
-	        }
-	        return "redirect:/homepage_foruser";
+	    
+	    @RequestMapping(value = "/deletecart/{cartId}", method = RequestMethod.GET)
+	    public String delete(@PathVariable int cartId) {
+	        cartdao.delete(cartId);
+	        return "redirect:/cart";
 	    }
+	    
+	    
 }
+
