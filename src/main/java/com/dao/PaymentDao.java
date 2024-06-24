03d846ea6fd195d52101ee12e6c81e40761bfffa
IlgarRzayev <92079654+IlgarRzayev@users.noreply.github.com>
2024-06-24 13:08:47 +0300

    Update PaymentDao.java

--- src/main/java/com/dao/PaymentDao.java
+++ src/main/java/com/dao/PaymentDao.java
@@ -1,6 +1,9 @@
 package com.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.beans.Cart;
@@ -16,8 +19,16 @@ public class PaymentDao {
 	
 	
     public void addPayment(Payment payment) {
-        String sql = "INSERT INTO payment (userId, productId, cartId, quantity) VALUES (?, ?, ?)";
-        template.update(sql, payment.getUserId(), payment.getProductId(),payment.getCartId(), payment.getQuantity());
+        String sql = "INSERT INTO payment (userId, totalPrice) VALUES (?, ?)";
+        template.update(sql, payment.getUserId(), payment.getTotalPrice());
     } 
+    public int delete(int cartId){    
+        String sql="delete from payment where cartId="+cartId+"";    
+        return template.update(sql);    
+    }
+    public List<Payment> getPaymentsByUserId(int userId) {
+        String sql = "SELECT * FROM payment WHERE userId=?";
+        return template.query(sql, new Object[] { userId }, new BeanPropertyRowMapper<Payment>(Payment.class));
+    }
     
 }
