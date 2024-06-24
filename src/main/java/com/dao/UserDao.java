833a9e0bbd3af42fab9dd768bf77b9e2aaa1ed11
IlgarRzayev <92079654+IlgarRzayev@users.noreply.github.com>
2024-06-24 13:09:24 +0300

    Update UserDao.java

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -2,7 +2,9 @@ package com.dao;
 
 import java.sql.ResultSet;    
 import java.sql.SQLException;    
-import java.util.List;    
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;    
 import org.springframework.jdbc.core.JdbcTemplate;    
 import org.springframework.jdbc.core.RowMapper;    
@@ -20,8 +22,26 @@ public int save(User p) {
     return template.update(sql);    
 }
 
+public User getUserByEmailAndPassword(String email, String password) {
+    String sql = "select * from User where email=? and password=?";
+    try {
+        return template.queryForObject(sql, new Object[]{email, password}, new BeanPropertyRowMapper<>(User.class));
+    } catch (Exception e) {
+        return null;
+    }
+}
+public User getUserByEmail(String email) {
+    String sql = "select * from User where email=?";
+    try {
+        return template.queryForObject(sql, new Object[]{email}, new BeanPropertyRowMapper<>(User.class));
+    } catch (Exception e) {
+        return null;
+    }
+}
+
+
 public int update(User p){    
-    String sql="update User set name='" + p.getName() + "','" + p.getSurname() + "','" + p.getEmail() + "','" + p.getPassword() + "' where userId="+p.getId()+"";    
+    String sql="update User set name='" + p.getName() + "', surname='" + p.getSurname() + "', email='" + p.getEmail() + "',password='" + p.getPassword() + "' where userId="+p.getId()+"";    
     return template.update(sql);    
 }    
 
@@ -33,7 +53,7 @@ public User getUserById(int id){
     String sql="select * from User where userId=?";    
     return template.queryForObject(sql, new Object[]{id},new BeanPropertyRowMapper<User>(User.class));    
 }    
-public List<User> getEmployees(){    
+public List<User> getUsers(){    
     return template.query("select * from User",new RowMapper<User>(){    
         public User mapRow(ResultSet rs, int row) throws SQLException {    
         	User e=new User();    
