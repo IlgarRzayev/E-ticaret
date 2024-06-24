f79fdc9afbfa58394ad85d55f4b47c4682c44bae
IlgarRzayev <92079654+IlgarRzayev@users.noreply.github.com>
2024-06-24 13:07:44 +0300

    Update CartDao.java

--- src/main/java/com/dao/CartDao.java
+++ src/main/java/com/dao/CartDao.java
@@ -1,12 +1,16 @@
 package com.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.beans.Cart;
+import com.beans.User;
 
 public class CartDao {
 	@Autowired
@@ -18,25 +22,39 @@ public class CartDao {
 	
 	
     public void addCartItem(Cart cartItem) {
-        String sql = "INSERT INTO carts (userId, productId, quantity) VALUES (?, ?, ?)";
-        template.update(sql, cartItem.getUserId(), cartItem.getProductId(), cartItem.getQuantity());
+        String sql = "INSERT INTO carts (userId, productId, quantity, price) VALUES (?, ?, ?, ?)";
+        template.update(sql, cartItem.getUserId(), cartItem.getProductId(), cartItem.getQuantity(), cartItem.getPrice());
     } 
-    
+    public int delete(int cartId){    
+        String sql="delete from carts where cartId="+cartId+"";    
+        return template.update(sql);    
+    }
     
     public List<Cart> getCartItemsByUserId(int userId) {
-        String sql = "SELECT * FROM cart WHERE user_id=?";
+        String sql = "SELECT * FROM carts WHERE userId=?";
         return template.query(sql, new Object[] { userId }, new BeanPropertyRowMapper<Cart>(Cart.class));
     }
 
-    
-    public void removeCartItem(int cartId) {
-        String sql = "DELETE FROM carts WHERE cartId=?";
-        template.update(sql, cartId);
+    public void deleteByUserId(int id) {
+        String sql = "DELETE FROM carts WHERE userId = ?";
+        template.update(sql, id);
     }
-
-    
-    public void clearCartByUserId(int userId) {
-        String sql = "DELETE FROM carts WHERE userId=?";
-        template.update(sql, userId);
+    public void deleteByProductId(int userId, int productId) {
+        String sql = "DELETE FROM carts WHERE userId = ? AND productId = ?";
+        template.update(sql, userId, productId);
     }
+    
+    public List<Cart> getCarts(){    
+        return template.query("select * from Carts",new RowMapper<Cart>(){    
+            public Cart mapRow(ResultSet rs, int row) throws SQLException {    
+            	Cart c=new Cart();    
+                c.setCartId(rs.getInt(1));    
+                c.setUserId(rs.getInt(2));    
+                c.setProductId(rs.getInt(3));    
+                c.setQuantity(rs.getInt(4));
+                c.setPrice(rs.getDouble(5));
+                return c;    
+            }    
+        });
+}
 }
