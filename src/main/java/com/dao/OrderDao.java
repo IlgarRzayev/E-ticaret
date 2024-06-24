fb2c9e2f8a9b52c98ece25dcf9d88a562439b0fa
IlgarRzayev <92079654+IlgarRzayev@users.noreply.github.com>
2024-06-24 13:08:30 +0300

    Update OrderDao.java

--- src/main/java/com/dao/OrderDao.java
+++ src/main/java/com/dao/OrderDao.java
@@ -1,8 +1,13 @@
 package com.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.beans.Order;
 import com.beans.User;
@@ -16,7 +21,7 @@ public class OrderDao {
 	}
 	
 	public int addOrder(Order order) {
-		String sql = "insert into orders(productId, userId, count, paymentMethod) values('" + order.getProductId() + "','" + order.getUserId() + "','" + order.getCount() + "','" + order.getPaymentMethod() + "')";    
+		String sql = "insert into orders(userId, productId, totalPrice, paymentMethod) values('" + order.getUserId() + "','" + order.getProductId() + "','" + order.getTotalPrice() + "','" + order.getPaymentMethod() + "')";    
 	    return template.update(sql);
 	}
 	public Order getOrdersByUserId(int userId) {
@@ -27,5 +32,27 @@ public class OrderDao {
 	        return null;
 	    }
 	}
-}	
+	public int delete(int orderId){    
+	    String sql="delete from orders where orderId="+orderId+"";    
+	    return template.update(sql);    
+	}
 	
+	public Order getOrderById(int id){    
+	    String sql="select * from orders where orderId=?";    
+	    return template.queryForObject(sql, new Object[]{id},new BeanPropertyRowMapper<Order>(Order.class));    
+	}
+	public List<Order> getOrders(){    
+	    return template.query("select * from Orders",new RowMapper<Order>(){    
+	        public Order mapRow(ResultSet rs, int row) throws SQLException {    
+	        	Order o=new Order();    
+	            o.setOrderId(rs.getInt(1));  
+	            o.setProductId(rs.getInt(2));
+	            o.setUserId(rs.getInt(3));    
+	            o.setTotalPrice(rs.getDouble(4));
+	            o.setPaymentMethod(rs.getString(5));
+	            return o;    
+	        }    
+	    });
+}	
+}
+
