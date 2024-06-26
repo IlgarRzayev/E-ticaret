e6f1e86a5b637a304b5e5f1c95baaa0c4bce9c32
IlgarRzayev <92079654+IlgarRzayev@users.noreply.github.com>
2024-06-26 06:03:28 +0300

    Update PaymentDao.java

--- src/main/java/com/dao/PaymentDao.java
+++ src/main/java/com/dao/PaymentDao.java
@@ -11,24 +11,30 @@ import com.beans.Payment;
 
 public class PaymentDao {
 	@Autowired
-	JdbcTemplate template;    
-	    
-	public void setTemplate(JdbcTemplate template) {    
-	    this.template = template;    
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
 	}
-	
-	
-    public void addPayment(Payment payment) {
-        String sql = "INSERT INTO payment (userId, totalPrice) VALUES (?, ?)";
-        template.update(sql, payment.getUserId(), payment.getTotalPrice());
-    } 
+
+	@Autowired
+	public JdbcTemplate setJdbcTemplate(JdbcTemplate template) {
+		return this.jdbcTemplate = template;
+	}
+
+	 public int savePayment(Payment payment) {
+	        String sql = "INSERT INTO payments (userId, totalPrice) VALUES (?, ?)";
+	        return jdbcTemplate.update(sql, payment.getUserId(), payment.getTotalPrice());
+	    } 
+	 
     public int delete(int cartId){    
         String sql="delete from payment where cartId="+cartId+"";    
-        return template.update(sql);    
+        return jdbcTemplate.update(sql);    
     }
-    public List<Payment> getPaymentsByUserId(int userId) {
-        String sql = "SELECT * FROM payment WHERE userId=?";
-        return template.query(sql, new Object[] { userId }, new BeanPropertyRowMapper<Payment>(Payment.class));
+    public Payment getPaymentByUserId(int userId) {
+        String sql = "SELECT * FROM payments WHERE userId = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{userId}, new BeanPropertyRowMapper<>(Payment.class));
     }
     
 }
